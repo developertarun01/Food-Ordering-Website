1e618b6709f5ada4462d218b6e695ea9713c6ca7
developertarun01 <tarunbusinessmail@gmail.com>
2024-11-25 13:03:52 +0530

    Contact

--- src/context/StoreContext.jsx
+++ src/context/StoreContext.jsx
@@ -22,11 +22,11 @@ const StoreContextProvider = (props) => {
     let totalAmount = 0;
     for (const item in cartItems) {
       if (cartItems[item] > 0) {
-        let itemInfo = food_list.find((product) => product._id === item);
+        let itemInfo = food_list.map((product) => product._id === item);
         totalAmount += itemInfo.price * cartItems[item];
       }
     }
-    return totalAmount;
+    return (totalAmount += 0);
   };
 
   const contextValue = {
